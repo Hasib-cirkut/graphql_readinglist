6b37098baa1c53e6930e8180b3ef2b5748b5d879
Hasibul Huda <hasibulhuda1@gmail.com>
2020-01-25 23:16:38 +0600

    Added mutation

--- client/src/components/Books.js
+++ client/src/components/Books.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
-import { useQuery } from '@apollo/react-hooks'
+import { useQuery, useMutation } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
 
 import {Button} from 'semantic-ui-react'
-import { Loader, Card} from 'semantic-ui-react'
+import { Loader, Card, Segment, Form} from 'semantic-ui-react'
 
 
 const books = gql`
@@ -19,9 +19,40 @@ query{
   }
 `
 
+const addBooks = gql`
+
+mutation($name: String, $genre: String){
+    addBook(name: $name, genre: $genre){
+      name,
+      genre
+    }
+  }`
+
 const Books = ()=>{
 
     const {loading, error, data} = useQuery(books)
+    const [addInfo] = useMutation(addBooks)
+
+    const [Name, setName] = useState("");
+    const [Genre, setGenre] = useState("");
+
+
+    const nameHandle = event =>{
+        
+        setName(event.target.value)
+    }
+
+    const genreHandle = event =>{
+        setGenre(event.target.value)
+    }
+
+    const handleSubmit = event =>{
+        event.preventDefault()
+
+        addInfo({variables:{name: Name, genre: Genre}})
+
+    }
+
 
     if(loading){
         return(
@@ -29,21 +60,33 @@ const Books = ()=>{
         )
     }else{
         let tempData = []
-
+        
         data.books.map(book =>{
-            tempData.push({header: book.name,description: book.author.name, meta: book.genre})
+            if(book.author === null){
+                tempData.push({header: book.name,description: "Unknown", meta: book.genre})
+            }else{
+                tempData.push({header: book.name,description: book.author.name, meta: book.genre})
+            }
+            
         })
         
-        return(/*
-            <div>
-                {data.authors.map(author =>(
-                    <h1>{author.name}</h1>
-        ))}
-            
-            </div>
-            */
+        return(
 
-           <Card.Group items={tempData} style={{marginLeft: "2vw", marginTop: "2vhls"}}/>
+            <div>
+                <Card.Group items={tempData} style={{marginLeft: "2vw", marginTop: "2vh"}}/>
+
+                <Segment inverted>
+                    <Form inverted >
+                    <Form.Group widths='equal'>
+                        <Form.Input fluid name="bookname" onChange={nameHandle} placeholder='Book name' />
+                        <Form.Input fluid name="bookgenre" onChange={genreHandle} placeholder='Genre' />
+                    </Form.Group>
+                    <Button type='submit' onClick={handleSubmit}>Submit</Button>
+                    </Form>
+                </Segment>
+            </div>
+
+        
         )
     }
 }
